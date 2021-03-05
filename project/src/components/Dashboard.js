7daa0e46e629e272cfe813c3370fa47bd8463398
Maged Raslan <maged.ahmedr@gmail.com>
2021-03-05 14:53:24 +0200

    Fix panes

--- project/src/components/Dashboard.js
+++ project/src/components/Dashboard.js
@@ -1,56 +1,58 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Tab } from 'semantic-ui-react'
-
 class Dashboard extends Component {
     render() {
-        
         const { authedUser, questions, users } = this.props;
         return (
             <div>
-                <Tab menu={{ pointing: true }} panes={getPanes(questions, users, authedUser)[0]} />
+                <Tab menu={{ pointing: true }} panes={getPanes(questions, users, authedUser)} />
             </div>
         )
     }
 }
 
 function getPanes(questions, users, authedUser){
-    let tabs = ['answered', 'unanswered'];
-    return Object.keys(tabs).map( function(item){
-      let qs = getQuestions(item, questions, users, authedUser)
-      return Object.keys(qs).map( function(id){
-        return {
-          menuItem: id,
-          render: () =>
-            <Tab.Pane attacched="false">
-              <div>
+    let aqs = getAnsweredQuestions(questions, users, authedUser)
+    let uqs = getUnAnsweredQuestions(questions, users, authedUser)
+    let tabs = ['answered', 'Unanswered']
+    return tabs.map(function(tab) {
+      return {
+        menuItem: tab,
+        render: () =>
+        <Tab.Pane attached={false}>
+            {Object.keys(tab == 'answered' ? aqs : uqs).map( function(id){
+              return <div key={id}>
                 <p>
-                <b>Author {qs[id].author} : </b>
+                <b>Author {aqs[id].author} : </b>
                 <br></br>
-                <label>{qs[id].optionOne.text}</label>
-                <input type="radio" name={id} value={qs[id].optionOne.text}/>
+                <label>{aqs[id].optionOne.text}</label>
+                <input type="radio" name={id} value={aqs[id].optionOne.text}/>
                 <br></br>
-                <label>{qs[id].optionTwo.text}</label>
-                <input type="radio" name={id} value={qs[id].optionTwo.text}/>
+                <label>{aqs[id].optionTwo.text}</label>
+                <input type="radio" name={id} value={aqs[id].optionTwo.text}/>
                 </p>
               </div>
+            })}
             </Tab.Pane>
-        }
-      })
-    })
+      }
+    }) 
 }
 
-function getQuestions(item, questions, users, authedUser) {
+function getAnsweredQuestions(questions, users, authedUser) {
   const answeredIds = Object.keys(users[authedUser[0]].answers);
-  if(item === 'answered') {
-    return  Object.values(questions)
-      .filter(question => !answeredIds.includes(question.id))
-      .sort((a, b) => b.timestamp - a.timestamp);
-  }else {
-    return Object.values(questions)
+
+  return Object.values(questions)
+    .filter(question => !answeredIds.includes(question.id))
+    .sort((a, b) => b.timestamp - a.timestamp);
+}
+
+function getUnAnsweredQuestions(questions, users, authedUser) {
+  const answeredIds = Object.keys(users[authedUser[0]].answers);
+
+  return Object.values(questions)
       .filter(question => answeredIds.includes(question.id))
       .sort((a, b) => b.timestamp - a.timestamp);
-  }
 }
 
 function mapStateToProps({authedUser, users, questions}) {
