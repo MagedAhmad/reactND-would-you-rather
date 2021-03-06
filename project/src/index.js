7eac63d81fad36e0a8b237b4155a9e60006d4f51
Maged Raslan <maged.ahmedr@gmail.com>
2021-03-06 16:04:36 +0200

    Add debugging tool and fix replying to question

--- project/src/index.js
+++ project/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './components/App'
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import middleware from './middlewares'
 import 'semantic-ui-css/semantic.min.css'
 
-const store = createStore(reducer, middleware)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(middleware));
+
 
 ReactDOM.render(
     <Provider store={store}>
